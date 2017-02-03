a53852d2eb29180e2ce7b39fcc5478bf4ca0624b
cKeinsley <keinsleyChris@gmail.com>
2017-02-03 10:38:00 -0500

    Attempting to get server to display welcome page

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return app.send_static_file('welcome.html')
 
 
 # @app.route('/movie/<int:movie_id>', method=['GET'])
