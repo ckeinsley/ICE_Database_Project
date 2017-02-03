375af70cf447db12bd3a80e898d519ce46e44767
cKeinsley <keinsleyChris@gmail.com>
2017-02-03 09:33:41 -0500

    fixed syntax errors

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import flask
 import pypyodbc
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-connection = pypyodbc.connect('Driver={SQL Server};'
-							  'Server=boilermake.database.windows.net;'
-							  'Database=Python Web Talk;'
-							  'uid=Boilermake;pwd=IvoryHacks2017')
+# connection = pypyodbc.connect('Driver={SQL Server};'
+#   'Server=boilermake.database.windows.net;'
+#   'Database=Python Web Talk;'
+#   'uid=Boilermake;pwd=IvoryHacks2017')
+
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+    return 'Hello World!'
 
-@app.route('/movie/<int:movie_id>',method=['GET'])
-def get_movie(movie_id):
-	cursor=connection.cursor()
-	SQLCommand=("SELECT Title, Year, Rated from Movies"
-				"WHERE Id=?")
-	Values=[movie_id]
-	cursor.execute(SQLCommand,Values)
-	results=cursor.fetchone()
-	flask.json.jsonify(results)
 
-if__name__=='__main__':
-	app.run(host='0.0.0.0',port=8000,debug=True)
+# @app.route('/movie/<int:movie_id>', method=['GET'])
+# def get_movie(movie_id):
+    # cursor = connection.cursor()
+    # SQLCommand = ("SELECT Title, Year, Rated from Movies"
+                #   "WHERE Id=?")
+    # Values = [movie_id]
+    # cursor.execute(SQLCommand, Values)
+    # results = cursor.fetchone()
+    # flask.json.jsonify(results)
+if __name__ =="__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
