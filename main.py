97b186e4d4ec33fea0e0953e1068379e23302281
cKeinsley <keinsleyChris@gmail.com>
2017-02-09 20:10:54 -0500

    gotta make python happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, jsonify
 from __future__ import print_function
+from flask import Flask, request, render_template, jsonify
 import pypyodbc
 import sys
 
