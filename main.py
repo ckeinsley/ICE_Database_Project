673778a29b0deed9cd807c46e8747b406dc375b0
yuchnw <yuchn.w@gmail.com>
2017-02-10 12:11:14 -0500

    add [] to check

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def recipe_page():
 @APP.route('/OrderList')
 def orderList_page():
     cursor = CONNECTION.cursor()
-    squery = ("SELECT* FROM Check")
+    squery = ("SELECT* FROM [Check]")
     cursor.execute(squery)
     results = cursor.fetchall()
     rows = []
