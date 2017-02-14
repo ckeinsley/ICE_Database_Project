ead4aff49c1a3b24ff02dff3ac21bbc3eaff89ef
cKeinsley <keinsleyChris@gmail.com>
2017-02-14 08:37:35 -0500

    trying connection.commit

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ def order_page():
         sqlquer = "exec AddOrder " + str(guestnumber) + " , " + str(recipename) + " , " + str(quantity) + " , '' "
         cursor.execute(sqlquer)
         results = cursor.fetchall()
+        CONNECTION.commit()
         return results
 
 
