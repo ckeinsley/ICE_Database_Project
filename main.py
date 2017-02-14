bbe8e5032f12c916504a81fe321249eae4a1f554
cKeinsley <keinsleyChris@gmail.com>
2017-02-14 09:59:45 -0500

    pizza isn't a column name

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ def recipe_page():
     recipename = request.args.get('name')
     cursor = CONNECTION.cursor()
     squery = "SELECT RecipeName, Description, NutritionalInfo, CookTime, PictureURL " \
-            "FROM Recipe WHERE RecipeName = " + recipename
+            "FROM Recipe " \
+            "WHERE RecipeName = " + recipename
     cursor.execute(squery)
     result = cursor.fetchone()
     return render_template('Recipe.html', recipe=result[0])
