46a1db4287b942e4b4e72294571fc2a1237cd02e
yuchnw <yuchn.w@gmail.com>
2017-02-14 23:13:36 -0500

    try8

--- main.py
+++ main.py
@@ -76,8 +76,10 @@ def orderList_page():
 
 @APP.route('/Order', methods=['GET', 'POST', 'PUT'])
 def order_page():
+    guestnumber = request.args.get('guestnumber', '')
+    username = request.args.get('user', '')
+    date = request.args.get('time', '')
     if request.method == 'POST':
-        guestnumber = request.args.get('guestnumber', '')
         recipename = request.form.get('name')
         quantity = request.form.get('quantity')
         cursor = CONNECTION.cursor()
@@ -88,7 +90,6 @@ def order_page():
         CONNECTION.commit()
 
     if request.method == 'PUT':
-        guestnumber = request.args.get('guestnumber', '')
         username = request.args.get('user', '')
         date = request.args.get('time', '')
         recipename = request.form.get('name')
@@ -98,26 +99,8 @@ def order_page():
             (guestnumber) + "] , [" + str(recipename) + \
             "] , [" + str(quantity) + "] , '' "
         cursor.execute(sqlquer)
-        squery = "Select Orders.RecipeName, Quantity, (Price*Quantity) as Price " \
-            "From [Check], Orders, Recipe " \
-            "Where [Check].GuestNumber = Orders.GuestNumber " \
-            "And Orders.RecipeName = Recipe.RecipeName " \
-            "AND [Check].GuestNumber = " + guestnumber
-
-        squery2 = "Select Orders.GuestNumber, [Date/Time], TableNumber "  \
-            "From [Check], Orders " \
-            "Where [Check].GuestNumber = Orders.GuestNumber " \
-            "AND [Check].GuestNumber = " + guestnumber
-        cursor.execute(squery)
-        recipesOrdered = cursor.fetchall()
-
-        cursor.execute(squery2)
-        checkInfo = cursor.fetchone()
-        return render_template('Order.html', orderInfo=recipesOrdered, checkInfo=checkInfo)
-
-    guestnumber = request.args.get('guestnumber', '')
-    username = request.args.get('user', '')
-    date = request.args.get('time', '')
+        CONNECTION.commit()
+ 
     cursor = CONNECTION.cursor()
     squery = "Select Orders.RecipeName, Quantity, (Price*Quantity) as Price " \
         "From [Check], Orders, Recipe " \
