0338511b871394c5628d084059c0cd18683c5856
cKeinsley <keinsleyChris@gmail.com>
2017-02-15 22:04:31 -0500

    debug string again

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def menu_page():
         sqlquer = "exec UpdateDish " + \
             "'"+str(recipename) + "' , " + str(defaultprice) + \
             " , " + str(defaultrate) + " , '" + str(info) + "', '" + str(des) +"'," + str(defaulttime) + ", '" + str(img) + "'"
+        return sqlquer
         cursor.execute(sqlquer)
         CONNECTION.commit()
 
