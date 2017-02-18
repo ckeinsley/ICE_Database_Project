5283964013bc7f0764dd2e12a6c174ce8551e61e
cKeinsley <keinsleyChris@gmail.com>
2017-02-18 12:35:44 -0500

    testing user input

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def menu_page():
             "'"+str(recipename) + "' , '" + str(price) + \
             "' , '" + str(time) + "' , '" + str(info) + "', '" + str(des) +"'," + rate + ", '" + str(img) + "' "
         sqlquer = remove_sql_comments(sqlquer)
+        return sqlquer
         cursor.execute(sqlquer)
         CONNECTION.commit()
 
